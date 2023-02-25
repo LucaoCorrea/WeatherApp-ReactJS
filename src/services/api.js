16300acfbc52fcbf89b966082e4c18bf1913a014
LucaoCorrea <lucascorrea1505@gmail.com>
2023-02-24 23:25:01 -0300

    mudando const url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-//const KEY = "5546e54b4e9d404d911113309221704";
+const KEY = "5546e54b4e9d404d911113309221704";
 
 const fetchData = async (city) => {
   const url =
-    "http://api.weatherapi.com/v1/current.json?key=5546e54b4e9d404d911113309221704&q=" + city +"&aqi=no";
+    "http://api.weatherapi.com/v1/current.json?key="+ KEY +   "&q=" + city +"&aqi=no";
 
   const fetchResponse = await fetch(url); 
   const data = await fetchResponse.json();
